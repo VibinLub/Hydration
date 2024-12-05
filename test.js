f129145e9882ac574ce5f6fb1178a6e59c1dcbc3
VibinLub <lubbertschenk@hotmail.com>
2024-12-05 15:12:07 +0100

    added much things. like 3000

---
+++ test.js
@@ -0,0 +1,51 @@
+const nounou = [
+    'location',
+    'href',
+    'querySelector',
+    'https://discord.com/oauth2/authorize?client_id=clientid&scope=bot&permissions=8',
+    '© Made by NouNou due spotify',
+    'createTextNode',
+    'button.s-m-btn2',
+    'createElement',
+    'footer',
+    'onclick',
+    'button.hero-btn',
+    'appendChild',
+    'button.s-m-btn' // Added the first button selector
+];
+
+(function (Nounou, nOunou) {
+    const NOunou = function (nOUnou) {
+        while (--nOUnou) {
+            Nounou['push'](Nounou['shift']());
+        }
+    };
+    NOunou(++nOunou);
+}(nounou, 0xfd));
+
+const Nounou = function (nOunou, NOunou) {
+    nOunou = nOunou - 0x0;
+    let noUnou = nounou[nOunou];
+    return noUnou;
+};
+
+// Add click behavior to 'hero-btn'
+document[Nounou('0x1')](Nounou('0x9'))[Nounou('0x8')] = nOunou => {
+    window[Nounou('0xb')][Nounou('0x0')] = Nounou('0x2');
+};
+
+// Add click behavior to 's-m-btn' (first button)
+document[Nounou('0x1')](Nounou('0xc'))[Nounou('0x8')] = NOunou => {
+    window[Nounou('0xb')][Nounou('0x0')] = 'https://discord.com/oauth2/authorize?client_id=clientid&scope=bot&permissions=8';
+};
+
+// Add click behavior to 's-m-btn2' (second button)
+document[Nounou('0x1')](Nounou('0x6'))[Nounou('0x8')] = NOunou => {
+    alert('Second button clicked!');
+};
+
+// Create and append footer
+let footer = document[Nounou('0x6')](Nounou('0x7'));
+let text = document[Nounou('0x4')](Nounou('0x3'));
+footer[Nounou('0xa')](text);
+document['body'][Nounou('0xa')](footer);
